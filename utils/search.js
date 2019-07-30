5c4bece1e8e5ae907255d2da8fa30bf9674735c8
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2019-07-30 17:35:21 +0800

    Update search.js

--- utils/search.js
+++ utils/search.js
@@ -1,47 +1,33 @@
 import Question from '../ethereum/question';
-async function getSumMatching(filterArray, question){
-    let i, j;
-    const summary =await Question(question).methods.getSummary().call();
-
-    //let arraySum = summary[0].concat(" ",(await getString('Qm'+summary[1]))).toLowerCase().split(" ");
-    let arraySum = summary[0].toLowerCase().split(" ");
-    console.log(arraySum);
-    let  occurences = 0;
-    for (i=0;i<filterArray.length;i++){
-        for (j=0;j<arraySum.length;j++){
-            if (filterArray[i]===arraySum[j]) occurences++;
-        }
-    }
-    console.log(occurences);
-    return occurences;
-}
-
+import {getString} from './ipfs';
+import factory from '../ethereum/factory';
+const Fuse = require("fuse.js");
 async function search(value, questions){
-    console.log('Hello');
-    let array = value.toLowerCase().split(" ");
-    let i;
-    let filterArray = [];
-    for (i=0;i<array.length;i++){
-        if (array[i]!='') filterArray.push(array[i]);
-    } 
-
-    let arrayOccurrences = [];
-    for (i=0;i<questions.length;i++){
-        arrayOccurrences.push([await getSumMatching(filterArray,questions[i]),questions[i]]);
+    let list = [];
+    for (let i=0;i<questions.length;i++){
+        var summary = await Question(questions[i]).methods.getSummary().call();
+        list.push({'address':questions[i],'title':summary[0],'description':summary[1]});//await getString('Qm'+summary[1])});
     }
-
-    console.log(arrayOccurrences);
-
-    arrayOccurrences.sort((a,b)=>{return (b[0]-a[0])});
-    console.log('This is', value);
-    if (arrayOccurrences[0][0] == 0) return [];
-    let result = [];
-    let limit = 10;
-    if (questions.length<limit) limit = questions.length;
-    for (i=0;i<limit;i++){
-        if (arrayOccurrences[i][0]==0) break;
-        result.push(arrayOccurrences[i][1]);
+    let options = {
+        id:"address",
+        threshold:0.1,
+        shouldSort: true,
+        tokenize: true,
+        keys: [{
+            name:'title',
+            weight:0.7
+        },{
+            name:'description',
+            weight: 0.3
+        }]
     }
+    let fuse = new Fuse(list,options);
+    const result = fuse.search(value);
+    console.log(result);
+    /*let result=[];
+    for (let i =0;i<resultDict.length;i++){
+        result.push(resultDict[i]['address']);
+    }*/
     return result;
 }
-export {getSumMatching,search};
+export{search};
